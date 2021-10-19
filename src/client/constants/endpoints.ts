98e49a6e73a65a34737bd152510bd8e24aec50f3
Rishi Ghan <rishi.ghan@gmail.com>
2021-10-19 10:21:24 -0700

    🔧 Trying out a docker container name as socket hostname

--- src/client/constants/endpoints.ts
+++ src/client/constants/endpoints.ts
@@ -1,3 +1,3 @@
 export const COMICBOOKINFO_SERVICE_URI = "http://localhost:3080/api/comicvine/";
 export const API_BASE_URI = "http://localhost:8050/api/";
-export const SOCKET_BASE_URI = "http://localhost:8051/";
+export const SOCKET_BASE_URI = "http://ghost:8051/";
