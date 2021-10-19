c3a91ef39feeeccf94d730b2457d4a7560318852
Rishi Ghan <rishi.ghan@gmail.com>
2021-10-19 12:40:13 -0700

    🔧 Interpolating hostname of Docker Host

--- src/client/constants/endpoints.ts
+++ src/client/constants/endpoints.ts
@@ -1,3 +1,4 @@
 export const COMICBOOKINFO_SERVICE_URI = "http://localhost:3080/api/comicvine/";
 export const API_BASE_URI = "http://localhost:8050/api/";
-export const SOCKET_BASE_URI = "http://ghost:8051/";
+export const SOCKET_BASE_URI =
+  process.env.DOCKERHOST + ":8051" || "ws://localhost:8051";
