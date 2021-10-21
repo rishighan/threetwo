419962c75638667a415fcab05795bc3782717c2d
Rishi Ghan <rishi.ghan@gmail.com>
2021-10-21 01:16:22 -0700

    ❌ Removed a funky route from the frontend API

--- src/server/route/routes/importComics.routes.ts
+++ src/server/route/routes/importComics.routes.ts
@@ -1,12 +1,14 @@
 import router from "../router";
 import { Request, Response } from "express";
 import axios from "axios";
-import { IMPORT_SERVICE_BASE_URI } from "../../../client/constants/endpoints";
 
 router.route("/getComicCovers").post(async (req: Request, res: Response) => {
   typeof req.body === "object" ? req.body : {};
+  const importServiceURI = process.env.DOCKER_HOST
+    ? `http://${process.env.DOCKER_HOST}:3000/api/import`
+    : "http://localhost:3000/api/import";
   await axios.request({
-    url: `${IMPORT_SERVICE_BASE_URI}/processAndImportToDB`,
+    url: `${importServiceURI}/processAndImportToDB`,
     method: "POST",
     data: {
       extractionOptions: req.body.extractionOptions,
