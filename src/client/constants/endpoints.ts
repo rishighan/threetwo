42090122988d079e9a0af305568784d4d548aeb6
Rishi Ghan <rishi.ghan@gmail.com>
2021-11-01 16:28:04 -0700

    🔧 Fixed the process env var

--- src/client/constants/endpoints.ts
+++ src/client/constants/endpoints.ts
@@ -38,7 +38,7 @@ export const IMPORT_SERVICE_BASE_URI = hostURIBuilder({
 
 export const SOCKET_BASE_URI = hostURIBuilder({
   protocol: "http",
-  host: process.env.LIBRARY_SOCKET_HOST || "localhost",
+  host: process.env.UNDERLYING_HOSTNAME || "localhost",
   port: "3001",
   apiPath: `/`,
 });
