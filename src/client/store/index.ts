195d9431c408e35d1645724f2ae0f354d0ad0af6
Rishi Ghan <rishi.ghan@gmail.com>
2021-11-01 01:10:29 -0700

    ✏️Editing socket client config

--- src/client/store/index.ts
+++ src/client/store/index.ts
@@ -3,17 +3,22 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { createBrowserHistory } from "history";
 import thunk from "redux-thunk";
 import createRootReducer from "../reducers";
-import socketIO from "socket.io-client";
+import { io } from "socket.io-client";
 import socketIoMiddleware from "redux-socket.io-middleware";
 // import { SOCKET_BASE_URI } from "../constants/endpoints";
-const io = socketIO(`http://rook:3001`);
+// const socketConnection = io(`http://rook:3001`);
+
 export const history = createBrowserHistory();
 const configureStore = (initialState) => {
   const store = createStore(
     createRootReducer(history),
     initialState,
     compose(
-      applyMiddleware(socketIoMiddleware(io), thunk, routerMiddleware(history)),
+      applyMiddleware(
+        // socketIoMiddleware(socketConnection),
+        thunk,
+        routerMiddleware(history),
+      ),
       // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
   );
