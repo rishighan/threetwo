5bf9e88b4163cd7d105aa56f0c5ca34006820ea5
Rishi Ghan <rishi.ghan@gmail.com>
2022-03-30 11:01:03 -0700

    🔼 Upgraded to react 18 and react-dom 18

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,15 +3,16 @@ import { render } from "react-dom";
 import { Provider, connect } from "react-redux";
 import { HistoryRouter as Router } from "redux-first-history/rr6";
 import { store, history } from "./store/index";
+import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 const rootEl = document.getElementById("root");
+const root = createRoot(rootEl);
 
-render(
+root.render(
   <Provider store={store}>
     <Router history={history}>
       <App />
     </Router>
   </Provider>,
-  rootEl,
 );
