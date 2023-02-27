45bcb4102dc5fed0d48f15c14d57ef82ae9ca563
Rishi Ghan <rishi.ghan@gmail.com>
2023-02-27 13:52:05 -0500

    🔧 Cleaned up package.json and bumped airdcpp-apisocket

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import react from '@vitejs/plugin-react';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   // publicDir: './public',
   build: "esnext",
   plugins: [
-    // …
+    nodeResolve({
+      // browser: true
+      exportConditions: ['node'],
+    }),
     react({
       // Use React plugin in all *.jsx and *.tsx files
       include: '**/*.{jsx,tsx}',
