4e82bc73e1f873841925aaab5b0939a56a48d600
Rishi Ghan <rishi.ghan@gmail.com>
2023-09-12 05:17:02 -0400

    ⬆️ Bumped @dnd-kit deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "@storybook/addon-mdx-gfm"
   ],
   framework: {
     name: "@storybook/react-vite",
--- src/client/components/Settings/QbittorrentSettings/QbittorrentConnectionForm.tsx
+++ src/client/components/Settings/QbittorrentSettings/QbittorrentConnectionForm.tsx
@@ -5,28 +5,30 @@ import { getQBitTorrentClientInfo } from "../../../actions/settings.actions";
 
 export const QbittorrentConnectionForm = (): ReactElement => {
   const dispatch = useDispatch();
-  const torrents = useSelector((state: RootState) => state.settings.torrentsList)
+  const torrents = useSelector(
+    (state: RootState) => state.settings.torrentsList,
+  );
 
   useEffect(() => {
-	  dispatch(getQBitTorrentClientInfo());
-  }, [])
-  const handleSubmit  = () => {}
-  return (<>
-	 <pre> {JSON.stringify(torrents, null, 4)} </pre>
-   
-   <Form
-     onSubmit={handleSubmit}
-     // validate={}
-     /* initialValues={} */
-     render={({ handleSubmit }) => (
-       <form onSubmit={handleSubmit}>
-         <h2>Configure Qbittorrent</h2>
-         
-       </form>
-     )}
-   />
-  
-  </>);
+    dispatch(getQBitTorrentClientInfo());
+  }, []);
+  const handleSubmit = () => {};
+  return (
+    <>
+      <pre> {JSON.stringify(torrents, null, 4)} </pre>
+
+      <Form
+        onSubmit={handleSubmit}
+        // validate={}
+        /* initialValues={} */
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <h2>Configure Qbittorrent</h2>
+          </form>
+        )}
+      />
+    </>
+  );
 };
 
 export default QbittorrentConnectionForm;
