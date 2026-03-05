2b4ee716e3a5609927b814f2edf000bb37bad195
Rishi Ghan <rishi.ghan@gmail.com>
2026-03-05 11:55:46 -0500

    🪓 Removed apollo-client

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,11 +1,9 @@
 import React, { ReactElement, useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client/react";
 import { Navbar2 } from "./shared/Navbar2";
 import { ToastContainer } from "react-toastify";
 import "../assets/scss/App.css";
 import { useStore } from "../store";
-import { apolloClient } from "../graphql/client";
 
 export const App = (): ReactElement => {
   useEffect(() => {
@@ -13,11 +11,11 @@ export const App = (): ReactElement => {
   }, []);
   
   return (
-    <ApolloProvider client={apolloClient}>
+    <>
       <Navbar2 />
       <Outlet />
       <ToastContainer stacked hideProgressBar />
-    </ApolloProvider>
+    </>
   );
 };
 
